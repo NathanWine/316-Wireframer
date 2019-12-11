6a5074bef206637b8c69fa98d0e66ea130bfb610
Nathan <illseeyouafterclass@gmail.com>
2019-12-11 17:22:50 -0500

    Added scrolling capability to EditScreen and delete wireframe buttons for WireframeCard

--- src/components/edit_screen/EditScreen.js
+++ src/components/edit_screen/EditScreen.js
@@ -18,36 +18,37 @@ class EditScreen extends Component {
             return <React.Fragment />
 
         return (
-            <div className="grey lighten-3" style={{height: '600px', borderRadius: '0 0 10px 10px'}}>
-                <div className="row flex" style={{height: 'inherit'}}>
-                    <div className="col s2 grey z-depth-2" style={{borderRadius: '0 0 0 10px'}}>
+            <div className="purple" style={{ height: '600px', borderRadius: '0 0 10px 10px' }}>
+                <div className="row flex" style={{ height: 'inherit'}}>
+                    <div className="col s2 grey z-depth-2" style={{ borderRadius: '0 0 0 10px' }}>
                         This is the side bar
                     </div>
-                    <div className="edit_window col s8" style={{position: 'relative'}}>
-                        This is the main window
-                        <Draggable bounds="parent">
-                            <div style={{display: 'inline-block'}}>Edit Screen</div>
-                        </Draggable>
-                        <Draggable
-                            axis="x"
-                            bounds="parent"
-                            handle=".handle"
-                            defaultPosition={{ x: 0, y: 0 }}
-                            position={null}
-                            grid={[25, 25]}
-                            scale={1}
-                            onStart={this.handleStart}
-                            onDrag={this.handleDrag}
-                            onStop={this.handleStop}>
-                            <div style={{display: 'inline-block'}}>
-                                <div className="handle">Drag from here</div>
-                                <div>This readme is really dragging on...</div>
-                            </div>
-                        </Draggable>
-                        <div>{wireframe.name}</div>
-                        <div>{wireframe.width}</div>
+                    <div className="col s8 center-align drag_window" style={{ position: 'relative', overflow: 'auto', height: 'inherit' }}>
+                        <div className="grey lighten-3" style={{ height: wireframe.height, width: wireframe.width, display:'inline-block' }}>
+                            <Draggable bounds="parent">
+                                <div style={{ display: 'inline-block' }}>Edit Screen</div>
+                            </Draggable>
+                            <Draggable
+                                axis="x"
+                                bounds="parent"
+                                handle=".handle"
+                                defaultPosition={{ x: 0, y: 0 }}
+                                position={null}
+                                grid={[25, 25]}
+                                scale={1}
+                                onStart={this.handleStart}
+                                onDrag={this.handleDrag}
+                                onStop={this.handleStop}>
+                                <div style={{ display: 'inline-block' }}>
+                                    <div className="handle">Drag from here</div>
+                                    <div>This readme is really dragging on...</div>
+                                </div>
+                            </Draggable>
+                            <div>{wireframe.name}</div>
+                            <div>{wireframe.width}</div>
+                        </div>
                     </div>
-                    <div className="col s2 grey z-depth-2" style={{borderRadius: '0 0 10px 0'}}>
+                    <div className="col s2 grey z-depth-2" style={{ borderRadius: '0 0 10px 0' }}>
                         This is the edit bar
                     </div>
                 </div>
--- src/components/home_screen/WireframeCard.js
+++ src/components/home_screen/WireframeCard.js
@@ -1,13 +1,21 @@
 import React from 'react';
 
 class WireframeCard extends React.Component {
+    deleteList = (e) => {
+        e.stopPropagation();
+        e.preventDefault();
+        console.log("Deleting wireframe", this.props.wireframe);
+    }
 
     render() {
         const { wireframe } = this.props;
         return (
-            <div className="card z-depth-2 rounded grey lighten-4 todo-list-link hoverable">
-                <div className="card-content grey-text text-darken-4 item_card">
-                    <span className="card-title">{wireframe.name}</span>
+            <div className="card z-depth-2 rounded grey lighten-4 hoverable">
+                <div className="card-content grey-text text-darken-4 item_card row valign-wrapper">
+                    <span className="card-title col s8">{wireframe.name}</span>
+                    <a onClick={this.deleteList} className="waves-effect waves-light btn red accent-2 hoverable col s2 offset-s2">
+                        <i className="material-icons">remove_circle_outline</i>
+                    </a>
                 </div>
             </div>
         );
