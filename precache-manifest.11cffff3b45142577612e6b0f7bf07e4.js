edae318b4bfbf489ea9e2d82fbb9db1e694f56a1
Nathan <illseeyouafterclass@gmail.com>
2020-03-15 01:42:57 -0400

    Updates

---
+++ precache-manifest.11cffff3b45142577612e6b0f7bf07e4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b09fa0783bd5df04870d913a3ac38df0",
+    "url": "/Wireframer/index.html"
+  },
+  {
+    "revision": "3b1ea972e86bccd1f0e4",
+    "url": "/Wireframer/static/css/main.2910785e.chunk.css"
+  },
+  {
+    "revision": "03b097447e6160c61020",
+    "url": "/Wireframer/static/js/2.bcd88c2f.chunk.js"
+  },
+  {
+    "revision": "218ac82556007f422d6da6447d230733",
+    "url": "/Wireframer/static/js/2.bcd88c2f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3b1ea972e86bccd1f0e4",
+    "url": "/Wireframer/static/js/main.7cda1b5d.chunk.js"
+  },
+  {
+    "revision": "63b5df0ba727ff4fa961",
+    "url": "/Wireframer/static/js/runtime-main.7af2b8c1.js"
+  },
+  {
+    "revision": "4a0d5a293824b878235c3a21e2fff3ad",
+    "url": "/Wireframer/static/media/UMBXrPdOoHOnxExyjdBeai3dAw.4a0d5a29.woff2"
+  },
+  {
+    "revision": "5a7bd2923196703ffe3a091f9c6a393a",
+    "url": "/Wireframer/static/media/UMBXrPdOoHOnxExyjdBeaiLdA4M5.5a7bd292.woff2"
+  },
+  {
+    "revision": "e12c9c5a15a9e1ac8eaa94ef252c94a3",
+    "url": "/Wireframer/static/media/UMBXrPdOoHOnxExyjdBeaiPdA4M5.e12c9c5a.woff2"
+  }
+]);
